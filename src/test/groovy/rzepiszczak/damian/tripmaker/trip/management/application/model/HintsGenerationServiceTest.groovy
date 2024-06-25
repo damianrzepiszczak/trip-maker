3a529e415dd91b3feb0f1eb0325570d912c22b39
damian.rzepiszczak <damian.rzepiszczak@gmail.com>
2024-06-25 12:48:39 +0200

    daily hint generation unit test refactor

--- src/test/groovy/rzepiszczak/damian/tripmaker/trip/management/application/model/HintsGenerationServiceTest.groovy
+++ src/test/groovy/rzepiszczak/damian/tripmaker/trip/management/application/model/HintsGenerationServiceTest.groovy
@@ -2,7 +2,9 @@ package rzepiszczak.damian.tripmaker.trip.management.application.model
 
 import rzepiszczak.damian.tripmaker.common.Clock
 import rzepiszczak.damian.tripmaker.common.MockClock
+import rzepiszczak.damian.tripmaker.common.event.DomainEventPublisher
 import rzepiszczak.damian.tripmaker.trip.management.application.commands.AssignPlanCommand
+import rzepiszczak.damian.tripmaker.trip.management.application.commands.CreateNewTripCommand
 import rzepiszczak.damian.tripmaker.trip.management.infrastructure.persistence.TripPersistenceConfiguration
 import spock.lang.Specification
 
@@ -15,21 +17,23 @@ class HintsGenerationServiceTest extends Specification {
     private Clock clock = new MockClock(someDay)
     private Trips trips = new TripPersistenceConfiguration().tripRepository()
     private TripFactory tripFactory = new TripFactory(trips, new MockTripsSettings())
-    private HintsGenerationService hintsGenerationService = new HintsGenerationService(new HintsGenerator(clock), trips)
+    private HintsGenerator hintsGenerator = new HintsGenerator(clock)
+    private TripService tripService = new TripManagement(trips, clock, tripFactory, Mock(DomainEventPublisher), hintsGenerator)
+    private HintsGenerationService hintsGenerationService = new HintsGenerationService(hintsGenerator, trips)
 
     def 'should generate new daily hint'() {
         given: 'trip is created'
-            Trip trip = tripFactory.create(travelerId, "Paris", someDay, someDay.plusDays(5))
-            trips.save(trip)
+            TripId tripId = tripService.create(new CreateNewTripCommand(travelerId, "Paris", someDay, someDay.plusDays(5)))
         and: 'assign plan'
-            trip.generateTimeline(new AssignPlanCommand(trip.id).details)
+            tripService.assignPlan(new AssignPlanCommand(tripId))
         and: 'start trip'
-            trip.start(someDay)
+            tripService.start(tripId)
         when: 'new day begins'
             hintsGenerationService.generate(someDay.plusDays(1))
         then: 'daily hint should be generated'
-            trip.hints().size() == 1
-            trip.hints()[0].content == "Daily Paris hint"
+            Trip trip = trips.findById(tripId).get()
+            trip.hints().size() == 2
+            trip.hints()[1].content == "Daily Paris hint"
     }
 
     class MockTripsSettings implements TripsSettings {
